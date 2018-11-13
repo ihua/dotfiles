a253d1d935884d604446e8ff55dfb216eba58074
Xiao Hua <xiaohua@gzyouai.com>
2018-11-13 11:22:34 +0800

    update Vundle.vim related configuration

--- vim/setup.sh
+++ vim/setup.sh
@@ -7,8 +7,9 @@ fi
 curdir=`pwd`
 cd $HOME/.vim/bundle
 if [ ! -d $HOME/.vim/bundle/vundle ]; then
-    git clone https://github.com/gmarik/vundle.git
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 cd $curdir
 ln -s $curdir/vimrc $HOME/.vimrc
+vim -c ":PluginInstall"
